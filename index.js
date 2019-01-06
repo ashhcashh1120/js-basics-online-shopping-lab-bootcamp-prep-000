35310ba1416d28fe37d1cf521da290256157dbe5
Ashley Achibald <aarchibald20@gmail.com>
2019-01-06 07:39:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,25 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ //the price of each item should be randomly generated
+  var price = Math.floor(Math.random() * 100)
+  //push the new cart array with item Name & item Price
+   cart.push( {itemName: item, itemPrice: price})
+   //return <itemName> has been added to your cart.
+   return `${item} has been added to your cart.`
+
 }
 
 function viewCart() {
   // write your code here
+  //if cart is empty return Your shopping cart is empty.
+  if(!cart.length){
+    return 'Your shopping cart is empty.'
+  }
+  //loop over ever item in cart
+for(let i = 0; i < cart.length; i++){
+
+}
 }
 
 function total() {
